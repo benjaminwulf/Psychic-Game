d4cf5746d467fce729c196458494f3229f9402dd
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-05-26 03:31:04 -0600

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Begining variables
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var letterToGuess; //used
+var letterToGuess;
 
 var wins;
 var losses;
@@ -12,23 +12,36 @@ var guessesSoFar = [];
 document.querySelector("#guesses-remaining").innerHTML = guessesRemaining;
 
 // Computer chooses a random letter. It is saved at variable computerLetter.
-var computerLetter = function() {
-   letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+var computerRandomLetterGenerator = function () {
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
 };
-computerLetter(); // bww
+computerRandomLetterGenerator(); // bww
 console.log("Computer generated random letter: " + letterToGuess); //bww
 
 // User makes letter guess
 // Key is pressed. Calls a function that is called on keyboard event
 // Stored as variable userGuess
-document.onkeydown = function(event) {
-   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-   console.log("User just guessed: " + userGuess);
+document.onkeydown = function (event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log("User just guessed: " + userGuess);
 
-   guessesSoFar.push(userGuess + ", ");
-   document.querySelector("#guesses-so-far").innerHTML = guessesSoFar
+    guessesSoFar.push(userGuess + " ");
+    document.querySelector("#guesses-so-far").innerHTML = guessesSoFar
 
-// Countdown on guessesRemaining
+    // Countdown on guessesRemaining
     document.querySelector("#guesses-remaining").innerHTML = guessesRemaining--;
-// };
+
+    //Computer choice is compared to users choice for equality inside for loop
+    if (letterToGuess == userGuess) {
+    //If equal display text signifing winner!
+    document.querySelector("#winner-message").innerHTML = "We have a WINNER!";
+    } 
+    else if (letterToGuess != userGuess) {
+    document.querySelector("#press-key-to-continue").innerHTML = "Press another letter to continue";
+    } else if (guessesRemaining == 0){
+    docuement.querySelecter("#loser-message").innerHTML = "Psyche... You lose!";
+    }
+    else {
+    alert("Something went terribly wrong. Please reload page.")
+    }
 };
