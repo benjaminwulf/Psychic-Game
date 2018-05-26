12d408dd810c923573a0adc2029de0c08c166992
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-05-26 04:37:57 -0600

    all done. could use more css. need document.querySelector messages to reset....

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,8 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var letterToGuess;
 
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var guessesRemaining = 9;
 document.querySelector("#guesses-remaining").innerHTML;
 var guessesSoFar = [];
@@ -11,11 +11,19 @@ var guessesSoFar = [];
 // HTML setup of page
 document.querySelector("#guesses-remaining").innerHTML = guessesRemaining;
 
+// Reset of game
+var resetGame = function() {
+    computerRandomLetterGenerator();
+    guessesRemaining = 9;
+    guessesSoFar = [];
+    document.querySelector("#reset-message").innerHTML = "Starting next round!";
+};
+
 // Computer chooses a random letter. It is saved at variable computerLetter.
 var computerRandomLetterGenerator = function () {
     letterToGuess = letters[Math.floor(Math.random() * letters.length)];
 };
-computerRandomLetterGenerator(); // bww
+computerRandomLetterGenerator();
 console.log("Computer generated random letter: " + letterToGuess); //bww
 
 // User makes letter guess
@@ -34,10 +42,16 @@ document.onkeydown = function (event) {
     //Computer choice is compared to users choice for equality inside for loop
     if (letterToGuess == userGuess) {
         //If equal display text signifing winner!
+        wins++;
         document.querySelector("#winner-message").innerHTML = "We have a WINNER!";
-    } else if (letterToGuess !== userGuess && guessesRemaining >= 0) {
+        document.querySelector("#wins").innerHTML = wins;
+        resetGame();
+    } else if (letterToGuess !== userGuess && guessesRemaining >= 1) {
         document.querySelector("#press-key-to-continue").innerHTML = "Press another letter to continue";
-    } else { 
+    } else if (letterToGuess !== userGuess && guessesRemaining <= 0) {
+        losses++;
         document.querySelector("#loser-message").innerHTML = "Psych... you lose!"
+        document.querySelector("#losses").innerHTML = losses;
+        resetGame();
     }
 };
