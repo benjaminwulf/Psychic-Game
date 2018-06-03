3ab3b8b8127767e890855d082a605a838bae9297
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-06-03 02:45:57 -0600

    working on enhancing hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // Begining variables
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var letterToGuess;
+var userGuess;
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 9;
@@ -8,14 +9,12 @@ var guessesSoFar = [];
 document.querySelector("#guesses-remaining").innerHTML = guessesRemaining;
 
 // Reset of game
-var resetGame = function() {
-    // computerRandomLetterGenerator();
+function resetGame() {
+    computerRandomLetterGenerator();
     guessesRemaining = 9;
     document.querySelector("#guesses-remaining").innerHTML = guessesRemaining
     guessesSoFar = [];
     document.querySelector("#guesses-so-far").innerHTML = guessesSoFar;
-    document.querySelector("#loser-message").innerHTML = " ";
-    document.querySelector("#winner-message").innerHTML = " ";
 };
 
 // Computer chooses a random letter. It is saved at variable computerLetter.
@@ -25,42 +24,65 @@ var computerRandomLetterGenerator = function () {
 computerRandomLetterGenerator();
 console.log("Computer generated random letter: " + letterToGuess); //bww
 
-// KEYBOARD PROMPT FOR MOBILE 
-function getKeyboardViaPrompt() {
-    var person = prompt("Guess a letter to get started");
-    if (person != null) {
-    var userGuess = person;
-    } else if (userAction == null) {
-        document.onkeydown = function (event) {
-     userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log("User just guessed: " + userGuess);    }
-};
 
-// Key is pressed. Calls a function that is called on keyboard event
-    // document.onkeydown = function (event) {
-    // userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    // console.log("User just guessed: " + userGuess);
-
+function compareFunc() {
     guessesSoFar.push(userGuess + " ");
-    document.querySelector("#guesses-so-far").innerHTML = guessesSoFar
 
-    // Countdown on guessesRemaining
-    document.querySelector("#guesses-remaining").innerHTML = guessesRemaining--;
-
-    //Computer choice is compared to users choice for equality inside for loop
     if (letterToGuess == userGuess) {
         //If equal display text signifing winner!
         wins++;
-        document.querySelector("#winner-message").innerHTML = "We have a WINNER!";
         document.querySelector("#wins").innerHTML = wins;
         resetGame();
-    } else if (letterToGuess !== userGuess && guessesRemaining == 0) {
+    } else if (letterToGuess !== userGuess && guessesRemaining > 0) {
+        document.querySelector("#guesses-so-far").innerHTML = guessesSoFar;
+        guessesRemaining--;
+        document.querySelector("#guesses-remaining").innerHTML = guessesRemaining--;
+        }    
+     else {
         losses++;
         document.querySelector("#loser-message").innerHTML = "Psych... you lose!"
         document.querySelector("#losses").innerHTML = losses;
         resetGame();
     }
 };
-resetGame(); //resetGame
 
-///=================Work on reset
+// TAKING KEYPRESS
+document.onkeydown = function (event) {
+    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    compareFunc(); 
+};
+
+// KEYBOARD PROMPT FOR MOBILE 
+function getKeyboardViaPrompt() {
+    var person = prompt("Guess a letter to get started");
+    if (person != null) {
+        userGuess = person;
+    } else {
+        console.log("input is null " + userGuess);
+        
+    }
+};
+
+//==================== OLD =============================
+// function doStuff(userGuess) {
+//         guessesSoFar.push(userGuess + " ");
+//         document.querySelector("#guesses-so-far").innerHTML = guessesSoFar
+
+//         // Countdown on guessesRemaining
+//         document.querySelector("#guesses-remaining").innerHTML = guessesRemaining--;
+
+//         //Computer choice is compared to users choice for equality inside for loop
+//         if (letterToGuess == userGuess) {
+//             //If equal display text signifing winner!
+//             wins++;
+//             document.querySelector("#winner-message").innerHTML = "We have a WINNER!";
+//             document.querySelector("#wins").innerHTML = wins;
+//             resetGame();
+//         } else if (letterToGuess !== userGuess && guessesRemaining == 0) {
+//             losses++;
+//             document.querySelector("#loser-message").innerHTML = "Psych... you lose!"
+//             document.querySelector("#losses").innerHTML = losses;
+//             resetGame();
+//         }
+//     }
+// };
